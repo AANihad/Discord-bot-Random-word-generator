e3dfd3f5c860cb7cf69867676ed46988b2faaa39
AANihad <68671314+AANihad@users.noreply.github.com>
2022-04-26 01:36:08 +0100

    Channel restriction

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
     // token: auth.token,
-	token: process.env.DISCORD_RWG_BOT,
+	token: "OTY3NDQ0NDc1NzI1MDkwODE2.YmQY4Q.v81OgdvNu7WHpLe8J-WbDo9wVng",
     autorun: true
 });
 
@@ -23,37 +23,37 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    bot.sendMessage({
+        to: "general",
+        message: "I'm ready"})
 });
 
 var blockedUsers=[];
-client.on('interactionCreate', interaction => {
-	if (blockedUsers.includes(interaction.user.id)) return;
-});
-
 bot.on('message', function (user, userID, channelID, message, evt) {
 
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if ((channelID == "general")&&(message.substring(0, 1) == '!')) {
+    if ((channelID == "wheel")&&(message.substring(0, 1) == '!')) {
     // if (message.content.startsWith(prefix) && (channelID == "general")){
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         args = args.splice(1);
         switch(cmd) {
             case 'spin':
-                const u = await client.users.fetch(userID);
-                if (blockedUsers.includes(userID))
-                    u.send("You already spinned the wheel")
-                else{
-                    u.send("||"+randomWords({ exactly: 3, join: " - " })+"||");
+                // u = client.users.fetch(userID);
+                // if (blockedUsers.includes(userID))
+                    // u.send("You already spinned the wheel")
+                // else{
+                    // u.send("||"+randomWords({ exactly: 3, join: " - " })+"||");
                     
-                    // bot.sendMessage({
-                    //     to: channelID,
-                    //     message: (randomWords({ exactly: 3, join: " - " }))
-                    // });
+                    bot.sendMessage({
+                        to: channelID,
+                        message: (randomWords({ exactly: 3, join: " - " }))
+                    });
                     blockedUsers.add(userID)
-                }
-            break;
+                break;
+                //}
+            
         }
     }
 });
